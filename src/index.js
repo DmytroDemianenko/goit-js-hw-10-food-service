43fc0d0ad673fecb70b3a0de9dc5e6f4a01522f4
Dmytro Demianenko <83134601+DmytroDemianenko@users.noreply.github.com>
2021-10-31 15:26:02 +0100

    a

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './menu.json';
+import './partiales/menu.hbs';
+import menuTpl from './src/partiales/menu.hbs';
+const menuEl = document.querySelector('.js-menu');
+
+function creatMenuList() {
+    return 
+}
+menuEl.insertAdjacentElement('beforeend', creatMenuList)
