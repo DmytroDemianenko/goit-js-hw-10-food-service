73ce1524d7592812d87491922486d3e577b52cbd
Dmytro Demianenko <83134601+DmytroDemianenko@users.noreply.github.com>
2021-10-31 17:52:46 +0100

    s

--- src/index.js
+++ src/index.js
@@ -1,9 +1,36 @@
-import './menu.json';
-import './partiales/menu.hbs';
-import menuTpl from './src/partiales/menu.hbs';
-const menuEl = document.querySelector('.js-menu');
+import '../src/styles.css';
+import createMenu from './partiales/menu.hbs';
+import menu from './js/menu.json';
 
-function creatMenuList() {
-    return 
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const refs = {
+    menuList: document.querySelector('.js-menu'),
+    toggleButton: document.querySelector('.theme-switch__toggle'),
+    body: document.body,
+};
+const theme = localStorage.getItem('Theme');
+   ( function () {
+        refs.menuList.insertAdjacentHTML('beforeend', createMenu(menu));
+       refs.body.classList.add(theme ? theme:Theme.LIGHT);
+       refs.toggleButton.checked = theme === Theme.DARK;
+    })();
+
+
+
+refs.toggleButton.addEventListener('click', onChandeTheme);
+function onChandeTheme(event) {
+    if (event.target.checked) {
+        toggleTheme(Theme.DARK,Theme.LIGHT)  
+    } else {
+        toggleTheme(Theme.LIGHT,Theme.DARK) 
+    }
 }
-menuEl.insertAdjacentElement('beforeend', creatMenuList)
+function toggleTheme(add,rem) {
+     refs.body.classList.add(add);
+    refs.body.classList.remove(rem);
+    localStorage.setItem('Theme', add);
+}
